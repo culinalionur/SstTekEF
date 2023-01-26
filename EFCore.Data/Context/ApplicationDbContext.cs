848955aa46e8fa21911b1770a5b36365b33cd243
culinalionur <onurakinci53@gmail.com>
2023-01-26 16:04:20 +0300

    Add project files.

---
+++ EFCore.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFCore.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+       
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer();
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("dbo");
+            
+
+            modelBuilder.Entity<Student>().Property(i => i.FirstName)
+                .HasColumnName("first_name")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(i => i.Id)
+                .HasColumnName("id")
+                .HasColumnType("int")
+                .UseIdentityColumn()
+                .IsRequired();
+            });
+        }
+    }
+}
---
+++ EFCore.Data/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String Author { get; set; }
+        public String Name { get; set; }
+
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
