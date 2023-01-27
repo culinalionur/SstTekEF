b5925ce1a19d1a56685f13261f460f2940b45318
culinalionur <onurakinci53@gmail.com>
2023-01-27 23:16:21 +0300

    Without Migration

--- EFCore.Data/Context/ApplicationDbContext.cs
+++ EFCore.Data/Context/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EFCore.Common;
 using EFCore.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,10 @@ namespace EFCore.Data.Context
 {
     public class ApplicationDbContext : DbContext
     {
-       
+       public ApplicationDbContext()
+        {
+
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
@@ -19,27 +23,71 @@ namespace EFCore.Data.Context
         {
             if(!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer();
+                optionsBuilder.UseSqlServer(StringConstants);
             }
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("dbo");
-            
-
-            modelBuilder.Entity<Student>().Property(i => i.FirstName)
-                .HasColumnName("first_name")
-                .HasColumnType("nvarchar")
-                .HasMaxLength(100);
 
             modelBuilder.Entity<Student>(entity =>
             {
-                entity.Property(i => i.Id)
-                .HasColumnName("id")
-                .HasColumnType("int")
-                .UseIdentityColumn()
-                .IsRequired();
+                entity.ToTable("students");
+
+                entity.Property(i => i.Id).HasColumnName("id").HasColumnType("int").UseIdentityColumn().IsRequired();
+                entity.Property(i => i.FirstName).HasColumnName("first_name").HasColumnType("nvarchar").HasMaxLength(250);
+                entity.Property(i => i.LastName).HasColumnName("last_name").HasColumnType("nvarchar").HasMaxLength(250);
+                entity.Property(i => i.Number).HasColumnName("number");
+                entity.Property(i => i.BirthDate).HasColumnName("birth_date");
+                entity.Property(i => i.AddressId).HasColumnName("address_id");
+
+                entity.HasMany(i => i.Books)
+                    .WithOne(i => i.Student)
+                    .HasForeignKey(i => i.StudentId)
+                    .HasConstraintName("student_book_id_fk");
+
             });
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.ToTable("teachers");
+
+                entity.Property(i => i.Id).HasColumnName("id").UseIdentityColumn();
+                entity.Property(i => i.FirstName).HasColumnName("first_name").HasColumnType("nvarchar").HasMaxLength(100);
+                entity.Property(i => i.LastName).HasColumnName("last_name").HasColumnType("nvarchar").HasMaxLength(100);
+                entity.Property(i => i.BirthDate).HasColumnName("birth_date");
+            });
+
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.ToTable("courses");
+
+                entity.Property(i => i.Id).HasColumnName("id").UseIdentityColumn();
+                entity.Property(i => i.Name).HasColumnName("name").HasColumnType("nvarchar").HasMaxLength(100);
+                entity.Property(i => i.IsActive).HasColumnName("is_active");
+            });
+
+            modelBuilder.Entity<StudentAddress>(entity =>
+            {
+                entity.ToTable("student_addresses");
+
+                entity.Property(i => i.Id).HasColumnName("id").UseIdentityColumn().ValueGeneratedOnAdd();
+                entity.Property(i => i.City).HasColumnName("city").HasMaxLength(50);
+                entity.Property(i => i.District).HasColumnName("district").HasMaxLength(100);
+                entity.Property(i => i.Country).HasColumnName("country").HasMaxLength(50);
+                entity.Property(i => i.FullAddress).HasColumnName("full_address").HasMaxLength(1000);
+
+                entity.HasOne(i => i.Student)
+                    .WithOne(i => i.Address)
+                    .HasForeignKey<Student>(i => i.AddressId)
+                    .IsRequired(false)
+                    .HasConstraintName("student_address_student_id_fk");
+            });
+
+
+
+            base.OnModelCreating(modelBuilder);
+        
         }
     }
 }
---
+++ EFCore.Data/Context/ApplicationDbContextDesignTimeFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Data.Context
+{
+    public class ApplicationDbContextDesignTimeFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var context = new ApplicationDbContext();
+            return context;
+
+        }
+    }
+}
